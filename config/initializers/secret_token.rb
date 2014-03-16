f22874eb670c9618ef696d57aca14edefc2b41b3
Ashley Edwards <ashley.elize@gmail.com>
2014-03-16 11:00:00 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tripwebsite::Application.config.secret_key_base = '11e958ff0c437a42db2653f9c547859ca6f17aa553d2644808ed309cdcda486b30d2cd80bffde40047be919d6414cc78ea3fca0f21792d4341259c9c5d771797'
