4341aa9e1ca127e04ae052f3753dca085c6be7a8
Ashley Edwards <ashley.elize@gmail.com>
2014-03-16 19:54:48 -0400

    fix config file

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,4 @@
 require File.expand_path('../boot', __FILE__)
-config.assets.initialize_on_precompile = false
 
 require 'rails/all'
 
@@ -20,5 +19,8 @@ module Tripwebsite
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.assets.initialize_on_precompile = false
+
   end
 end
